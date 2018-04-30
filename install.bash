8c1b54a982be2b3b8ee8c58b7d9dd3d46f6a517f
karl <karl.van.rompaey@hotmail.com>
2018-04-30 16:15:35 +0100

    fix(install): fix multi line command

--- install.bash
+++ install.bash
@@ -5,10 +5,9 @@ if [ "$#" -ne 1 ]; then
     exit 1;
 fi
 
-rm -rf .git && \\
-sed -i -e "s/ts-boiler/$1/g" package.json && \\
-rm -f rm package.json-e && \\
-git init && \\
-yarn install && \\
-git remote add https://github.com/kavaro/$1 && \\
+rm -rf .git && \
+sed -i -e "s/ts-boiler/$1/g" package.json && \
+rm -f rm package.json-e && \
+git init && yarn install && \
+git remote add https://github.com/kavaro/$1 && \
 rm install.bash
